3820403a391127c83724ca5319dcd3e9cb822aa4
flatedex <flatedex@mail.ru>
2023-08-28 17:29:48 +0300

    added modal windows and routing points

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict'
 
+const Pool = require('pg').Pool;;
+
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.POSTGRESQL_USER,
+    host: process.env.POSTGRESQL_HOST,
+    database: process.env.POSTGRESQL_DB,
+    password: process.env.POSTGRESQL_PASSWORD,
+    port: process.env.POSTGRESQL_PORT,
+});
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
 const PORT = 8050;
-const HOST = "0.0.0.0";
+const HOST = "localhost";
 
 const app = express();
 
@@ -19,8 +31,22 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.post('/', (req, res) => {
-    
+app.post('/sms', async (req, res) => {
+    const result = await pool.query('');
+    res.sendStatus(200);
+});
+app.post('/telegram', async (req, res) => {
+    const { text, buttonCount } = req.body;
+    const result = await pool.query('INSERT INTO mailing (mailing_text, button_count)');
+    res.sendStatus(200);
+});
+app.post('/whatsapp', async (req, res) => {
+    const result = await pool.query('');
+    res.sendStatus(200);
+});
+app.post('/vkontakte', async (req, res) => {
+    const result = await pool.query('');
+    res.sendStatus(200);
 });
 
 app.listen(PORT, HOST, () => {
